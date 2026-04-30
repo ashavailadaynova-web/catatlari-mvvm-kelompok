d00979a1864fd8beba1d626ad7aaf778dd4c22d2
Daynova Ashavaila Ramadhani <ashavailadaynova@gmail.com>
2026-04-30 22:41:46 +0700

    initial project from dosen

--- app/src/main/java/com/upn/catatlari/viewmodel/runViewModel.kt
+++ app/src/main/java/com/upn/catatlari/viewmodel/runViewModel.kt
@@ -7,25 +7,30 @@ import com.upn.catatlari.model.Run
 
 class RunViewModel : ViewModel() {
 
-    private val runList = listOf(
+    val runList = listOf(
         Run(runDate = "22 Mei 2026", runDistance = 1, runDuration = 3),
         Run(runDate = "23 Mei 2026", runDistance = 1, runDuration = 3),
         Run(runDate = "24 Mei 2026", runDistance = 1, runDuration = 3)
     )
 
-    private val runListLiveData = MutableLiveData<List<Run>>(runList)
-
-    val runHistory: LiveData<List<Run>> = runListLiveData
+    private val runListLiveData = MutableLiveData(runList) // variabel yang berfungsi mengatur perubahan data
+    var runHistory : LiveData<List<Run>> = runListLiveData // variabel yang dipakai untuk dipanggil class lain
 
     // CREATE
     fun addRun(run: Run) {
-        // ambil nilai data maka pakek value, trs orempty kalo kosong maka dikosong kao engga ya muncul mutabele  list nya
+//        if (runListLiveData.value == null) {
+//
+//        }
+
         val currentList = runListLiveData.value.orEmpty().toMutableList()
         currentList.add(run)
-        // ini kayak nge update gitu.
         runListLiveData.value = currentList
     }
+
     // READ
+
     // UPDATE
+
     // DELETE
+
 }
