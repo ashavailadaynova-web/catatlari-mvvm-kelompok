8a8259c6e3410c37fe4d06da53e3deb2a0448dad
Daynova Ashavaila Ramadhani <ashavailadaynova@gmail.com>
2026-05-03 14:35:21 +0700

    fix: ganti Room ke SQLite + perbaikan User & Run

--- app/src/main/java/com/upn/catatlari/model/Run.kt
+++ app/src/main/java/com/upn/catatlari/model/Run.kt
@@ -1,11 +1,6 @@
 package com.upn.catatlari.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "run_table")
 data class Run(
-    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val runDate: String,
     val runDuration: Int,
